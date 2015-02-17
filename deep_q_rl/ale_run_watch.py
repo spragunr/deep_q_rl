081938fcb33d91d023527a85bdd848cfac57efd1
Nathan Sprague <nathan.r.sprague@gmail.com>
2015-02-17 11:45:52 -0500

    Add optional ROM argument to ale_run_watch.py

--- deep_q_rl/ale_run_watch.py
+++ deep_q_rl/ale_run_watch.py
@@ -3,15 +3,22 @@ visualization turned on.
 
 Usage:
 
-ale_run_watch.py NETWORK_PKL_FILE
+ale_run_watch.py NETWORK_PKL_FILE [ ROM ]
 """
 import subprocess
 import sys
 
-command = ['./ale_run.py', '--glue-port', '4097', '--steps-per-epoch', '0',
-           '--test-length', '10000', '--nn_file', sys.argv[1],
-           '--pause', '.03', '--display-screen']
+def run_watch():
+    command = ['./ale_run.py', '--glue-port', '4097', '--steps-per-epoch', '0',
+               '--test-length', '10000', '--nn_file', sys.argv[1],
+               '--pause', '.03', '--display-screen']
 
-p1 = subprocess.Popen(command)
+    if len(sys.argv) > 2:
+        command.extend(['--rom', sys.argv[2]])
 
-p1.wait()
+    p1 = subprocess.Popen(command)
+    
+    p1.wait()
+
+if __name__ == "__main__":
+    run_watch()
