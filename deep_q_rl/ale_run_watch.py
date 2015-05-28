ea184d9296e1eaf3bd223d2496377c9e0e8546e0
Nathan Sprague <nathan.r.sprague@gmail.com>
2015-05-28 09:11:14 -0400

    Update ale_run_watch.py to be consistent with new scripts.

--- deep_q_rl/ale_run_watch.py
+++ deep_q_rl/ale_run_watch.py
@@ -9,8 +9,9 @@ import subprocess
 import sys
 
 def run_watch():
-    command = ['./ale_run.py', '--glue-port', '4097', '--steps-per-epoch', '0',
-               '--test-length', '10000', '--nn_file', sys.argv[1],
+    command = ['./run_nature.py', '--glue-port', '4097', 
+               '--steps-per-epoch', '0',
+               '--test-length', '10000', '--nn-file', sys.argv[1],
                '--pause', '.03', '--display-screen']
 
     if len(sys.argv) > 2:
