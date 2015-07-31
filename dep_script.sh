f8a74e87c5d673baa0360709ac5cfa2ffbffea31
Nathan Sprague <nathan.r.sprague@gmail.com>
2015-07-31 13:06:33 -0400

    Modify dependency script to avoid some system-level installs.

--- dep_script.sh
+++ dep_script.sh
@@ -3,7 +3,7 @@
 echo "==>dependencies setup for deep_q_rl"
 
 echo "==>updating current package..."
-#sudo apt-get update
+sudo apt-get update
 
 echo "==>installing Cython..."
 sudo apt-get install cython
@@ -17,8 +17,10 @@ sudo apt-get install python-matplotlib python-tk
 echo "==>installing Theano ..."
 # some dependencies ...
 sudo apt-get install python-numpy python-scipy python-dev python-pip python-nose g++ libopenblas-dev git
-sudo pip install --upgrade --no-deps git+git://github.com/Theano/Theano.git
+pip install --user --upgrade --no-deps git+git://github.com/Theano/Theano.git
 
+echo "==>installing Lasagne ..."
+pip install --user --upgrade https://github.com/Lasagne/Lasagne/archive/master.zip
 
 # Packages below this point require downloads. 
 mkdir build
@@ -32,8 +34,7 @@ sudo apt-get install libyaml-0-2 python-six
 git clone git://github.com/lisa-lab/pylearn2
 fi
 cd ./pylearn2
-sudo python setup.py develop
-sudo rm -r ~/.theano
+python setup.py develop --user
 cd ..
 
 if [ ! -d "./ALE" ]
@@ -46,18 +47,9 @@ sudo apt-get install libsdl1.2-dev libsdl-gfx1.2-dev libsdl-image1.2-dev cmake
 git clone https://github.com/mgbellemare/Arcade-Learning-Environment ALE
 cd ./ALE
 cmake -DUSE_SDL=ON -DUSE_RLGLUE=OFF .
-make
-sudo pip install .
+make -j2
+pip install --user .
 cd ..
-fi
-
-if [ ! -d "./Lasagne" ]
-then
-echo "==>installing Lasagne ..."
-
-git clone https://github.com/Lasagne/Lasagne.git
-cd ./Lasagne
-sudo python setup.py install
 fi
 
 echo "==>All done!"
